2605fdf6ab6d13a51e0f1afabbe59990a7f87058
Marcin Czekajski <czekajskimarcin@gmail.com>
2021-01-31 15:14:11 +0100

    basic webcomponent

---
+++ popupNotify.js
@@ -0,0 +1,18 @@
+const template = document.createElement("template");
+template.innerHTML = `
+    <style>
+    </style>
+    <div class="tooltip-container">
+        test
+    </div>
+`;
+
+class PopupNotify extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+}
+
+window.customElements.define('popup-notify', PopupNotify)
